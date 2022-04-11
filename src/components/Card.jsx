f5a6ea8c79eeb5f4fb8e8ab86c39944b95a58e42
unknown <ribeirojoaolucas68@gmail.com>
2022-04-11 17:42:44 -0300

    feat: add Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <>
+      <div className="container">
+        <p className="title">Join our community</p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
