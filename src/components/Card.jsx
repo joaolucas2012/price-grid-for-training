230e2c70a7914875da692b83e395421c733ae0da
unknown <ribeirojoaolucas68@gmail.com>
2022-04-12 20:28:53 -0300

    feat: add new divs with prices information

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,30 @@ const Card = () => {
   return (
     <div className="container">
       <div className="card">
-        <Header />
+        <div id="card-top">
+          <Header />
+          <div className="left-text">
+            <p className="green">30-day, hassle free money back guarantee</p>
+          </div>
+          <div className="left-text">
+            <p className="grey">
+              Gain access to our full library of tutorials along with expert
+              code reviews.
+            </p>
+            <p className="grey">
+              Perfect for any developers who are serious about honing their
+              skills.
+            </p>
+          </div>
+        </div>
+        <div id="card-bottom">
+          <div className="monthly-subscription">
+            <p>Monthly Subscription</p>
+          </div>
+          <div className="why-us">
+            <p>Why Us?</p>
+          </div>
+        </div>
       </div>
     </div>
   );
