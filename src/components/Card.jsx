0f15625f5bd4ea32ef7a1012f05b8334eee33c67
unknown <ribeirojoaolucas68@gmail.com>
2022-04-14 00:31:43 -0300

    feat: fix style and components organization

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 import "./Card.css";
 
-import Text from "./Text"
 import HalfContainer from "./HalfContainer";
-import Title from "./Title";
-import GreyText from "./GreyText";
 import Header from "./Header";
 
 const Card = () => {
@@ -15,27 +12,45 @@ const Card = () => {
         <div id="card-top">
           <Header />
           <div className="left-text">
-            <Text className="green">
-              30-day, hassle-free money back guarantee
-            </Text>
+            <p className="green">30-day, hassle-free money back guarantee</p>
           </div>
           <div className="left-text">
-            <GreyText>
+            <p className="font-13 grey">
               Gain access to our full library of tutorials along with expert
               code reviews.
-            </GreyText>
-            <GreyText>
+            </p>
+            <p className="font-13 grey">
               Perfect for any developers who are serious about honing their
               skills.
-            </GreyText>
+            </p>
           </div>
         </div>
-        <div>
-          <HalfContainer className="lightseagreen">
-            <Title className="left-text">Monthly Subscription</Title>
+        <div className="card-bottom">
+          <HalfContainer className="lightseagreen__background">
+            <div className="left-space">
+              <p className="lightcyan">Monthly Subscription</p>
+              <div id="monthly-price">
+                <h1 className="inline">$29</h1>
+                <p className="font-13 mediumturquoise inline">per month</p>
+              </div>
+              <p className="font-13 lightcyan">
+                Full access for less than $1 a day
+              </p>
+            </div>
           </HalfContainer>
-          <HalfContainer className="mediumturquoise">
-            <Title>Why Us?</Title>
+          <HalfContainer className="mediumturquoise__background">
+            <div className="left-space">
+              <p className="lightcyan">Why Us?</p>
+              <div id="info">
+                <p>Tutorials by industry experts</p>
+                <p>Peer and expert code review</p>
+                <p>Coding exercises</p>
+                <p>Access to our Github repos</p>
+                <p>Community forum</p>
+                <p>Flashcard decks</p>
+                <p>New videos every week</p>
+              </div>
+            </div>
           </HalfContainer>
         </div>
       </div>
--- src/components/Text.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Text = (props) => {
-  return <p className={props.className}>{props.children}</p>;
-};
-
-export default Text;
