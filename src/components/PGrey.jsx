784a2481d1173f1f1567c27a6d9206be13cf37bb
unknown <ribeirojoaolucas68@gmail.com>
2022-04-15 22:41:28 -0300

    feat: fix code organization

--- src/components/PGrey.jsx
+++ src/components/PGrey.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
 import "./PGrey.css";
 
 const PGrey = (props) => {
-    return ( 
-        <p className="p-grey">{props.children}</p>
-     );
-}
- 
+  return <p className="p-grey">{props.children}</p>;
+};
+
 export default PGrey;
